a1fe375d90df2a44ac0ee70d60e221a5a03dde15
Florian CATINAUD <fcatinau@e2r9p10.42.fr>
2021-05-19 12:34:25 +0200

    add main and push_swap

---
+++ push_swap.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fcatinau <fcatinau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/19 11:46:36 by fcatinau          #+#    #+#             */
+/*   Updated: 2021/05/19 12:16:07 by fcatinau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+// sa = move pos 1 et pos 2 de pile a SAUF SI POS < 1
+// sb = move pos 1 et pos 2 de pile b SAUF SI POS < 1
+// ss = sa + sb
+
+// --------------------------------------------------
+
+// pa = prends le 1er elem de b pour allez le mettre sur a SAUF SI B < 1
+// pb = prends le 1er elem de a pour allez le mettre sur b SAUF SI A < 1
+
+// --------------------------------------------------
+
+// ra = decale d'une postion vers le haut de tous les elements de la liste a + first passe last
+// rb = decale d'une postion vers le haut de tous les elements de la liste b + first passe last
+// rr = ra + rb;
+
+// --------------------------------------------------
+
+// rra = decale d'une position vers le bas tout les elements de la liste a, le dernier devient le premier;
+// rrb = decale d'une position vers le bas tout les elements de la liste b, le dernier devient le premier;
+// rrr = rra + rrb;
+
+#endif
