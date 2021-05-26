884f2b8f442f01f4ba5b80a2646e5774badb92a9
Florian CATINAUD <fcatinau@e2r9p10.42.fr>
2021-05-26 12:59:21 +0200

    modif solver

--- solver.c
+++ solver.c
@@ -6,7 +6,7 @@
 /*   By: fcatinau <fcatinau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/20 15:12:42 by fcatinau          #+#    #+#             */
-/*   Updated: 2021/05/25 18:56:09 by fcatinau         ###   ########.fr       */
+/*   Updated: 2021/05/25 19:09:03 by fcatinau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@ int	solver(t_swap *swap)
 		i = 0;
 	printf("tour %d \n", i);
 	i++;
-	while (j < 10000000)
-		j++;
+	//while (j < 10000000)
+	//	j++;
 	verif_two_first(swap);
 	verif_swap_list(swap);
 	verif_top_and_bot(swap);
