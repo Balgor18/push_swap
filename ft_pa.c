26eefb837ac9c198dc413b19d8b2c3409b2f2eb2
Florian CATINAUD <fcatinau@e2r9p10.42.fr>
2021-06-10 10:33:53 +0200

    modif name file of pas and pb

--- ft_pa.c
+++ ft_pa.c
@@ -1,29 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_pa.c                                            :+:      :+:    :+:   */
+/*   ft_pb.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fcatinau <fcatinau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/20 16:44:34 by fcatinau          #+#    #+#             */
-/*   Updated: 2021/06/10 10:32:07 by fcatinau         ###   ########.fr       */
+/*   Created: 2021/05/22 23:52:21 by fcatinau          #+#    #+#             */
+/*   Updated: 2021/06/10 10:31:55 by fcatinau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	pb(t_swap *s)
+void	pa(t_swap *s)
 {
 	int	i;
 
-	i = s->count.len_b + 1;
+	i = s->count.len_a + 1;
 	while (--i > 0)
-		s->b[i] = s->b[i - 1];
-	s->count.len_b++;
-	s->b[0] = s->a[0];
+		s->a[i] = s->a[i - 1];
+	s->count.len_a++;
+	s->a[0] = s->b[0];
 	i = -1;
-	s->count.len_a--;
-	while (++i < s->count.len_a)
-		s->a[i] = s->a[i + 1];
-	ft_putstr_fd("pb\n", 1);
+	s->count.len_b--;
+	while (++i < s->count.len_b)
+		s->b[i] = s->b[i + 1];
+	ft_putstr_fd("pa\n", 1);
 }
